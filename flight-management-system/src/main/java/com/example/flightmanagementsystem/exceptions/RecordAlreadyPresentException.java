d09385a512b95059ad7bfe3e1fdaa225fea7d5a4
Subha lakshmi S <74890926+subhaLakshmi-S@users.noreply.github.com>
2022-07-18 19:56:31 +0530

    Add files via upload

---
+++ flight-management-system/src/main/java/com/example/flightmanagementsystem/exceptions/RecordAlreadyPresentException.java
@@ -0,0 +1,12 @@
+package com.flight.project.exception;
+
+public class RecordAlreadyPresentException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public RecordAlreadyPresentException(String s) {
+		super(s);
+	}
+}
