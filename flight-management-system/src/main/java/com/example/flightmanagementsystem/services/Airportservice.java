92a9ea18e1b26dc91f26fe7330359c0f2e9f579c
Shanmuga lakshmi <67747282+sowshan@users.noreply.github.com>
2022-07-20 17:01:45 +0530

    Add files via upload

--- flight-management-system/src/main/java/com/example/flightmanagementsystem/services/Airportservice.java
+++ flight-management-system/src/main/java/com/example/flightmanagementsystem/services/Airportservice.java
@@ -1,19 +1,37 @@
-package com.example.flightmanagementsystem.services;
-
+package com.example.demo.service;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.entity.Airport;
+import com.example.demo.repo.Airportrepo;
+@Service
+public class Airportservice {
+ @Autowired
+ private Airportrepo airportrepo;
+ public List<Airport> viewAllAirport() {
+ return airportrepo.findAll();
+ }
+ public List<Airport> viewairportById(String airportId)  {
+ return airportrepo.findByairportId(airportId);
+ }
+ public List<Airport> viewairportByName(String airportName)  {
+	 return airportrepo.findByairportName(airportName);
+	 }
+ public int removeAirportId(String airportId) {
+  airportrepo.deleteById(airportId);
+  return 1;
+  }
+ public int removeAirportName(String airportName) {
+	  airportrepo.deleteByairportName(airportName);
+	  return 1;
+	  }
+ public Airport addAirport(Airport airport) {
+ return airportrepo.save(airport);
+ }
+ public Airport modifyAirport(Airport airport) {
+ return airportrepo.save(airport);
+ }
+ 
 
-
-
-import com.example.flightmanagementsystem.entity.Airport;
-import com.example.flightmanagementsystem.exceptions.AirportNotFoundException;
-
-
-public interface Airportservice {
-	
-	Airport addAirport(Airport airport);
-	Airport modifyAirport(Airport airport);
-	int removeAirport(String airportId) throws AirportNotFoundException;
-	public List<Airport> viewAllAirport();
-	public Airport viewByAirportId(String airportId);
 	
 }
