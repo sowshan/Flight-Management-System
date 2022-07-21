dbd39e4b7d7f40d0c7d0f4faff549fd16eee5e33
Shanmuga lakshmi <67747282+sowshan@users.noreply.github.com>
2022-07-21 09:29:23 +0530

    Add files via upload

--- flight-management-system/src/main/java/com/example/flightmanagementsystem/services/AirportService.java
+++ flight-management-system/src/main/java/com/example/flightmanagementsystem/services/AirportService.java
@@ -1,37 +1,66 @@
 package com.example.flightmanagementsystem.services;
+
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.example.flightmanagementsystem.entity.Airport;
 import com.example.flightmanagementsystem.repositories.AirportRepo;
-@Service
-public class Airportservice {
- @Autowired
- private AirportRepo airportrepo;
- public List<Airport> viewAllAirport() {
- return airportrepo.findAll();
- }
- public List<Airport> viewairportById(String airportId)  {
- return airportrepo.findByairportId(airportId);
- }
- public List<Airport> viewairportByName(String airportName)  {
-	 return airportrepo.findByairportName(airportName);
-	 }
- public int removeAirportId(String airportId) {
-  airportrepo.deleteById(airportId);
-  return 1;
-  }
- public int removeAirportName(String airportName) {
-	  airportrepo.deleteByairportName(airportName);
-	  return 1;
-	  }
- public Airport addAirport(Airport airport) {
- return airportrepo.save(airport);
- }
- public Airport modifyAirport(Airport airport) {
- return airportrepo.save(airport);
- }
- 
 
-	
-}
+@Service
+public class AirportService {
+	@Autowired
+	 private AirportRepo airportrepo;
+	 
+	public List<Airport> viewAllAirport() {
+	 return  (List<Airport>) airportrepo.findAll();
+	 }
+	 
+	public String viewairportById(String airportId)  {
+		if (airportrepo.existsById(airportId)) {
+			return "Airport Found";
+		}
+		else
+			return "AirportID Not Found";
+	 }
+	 
+	 public boolean removeAirportId(String airportId) {
+		 if (airportrepo.existsById(airportId)) {
+				Airport airport = airportrepo.getReferenceById(airportId);
+				airportrepo.deleteById(airportId);
+				return true;
+
+			} else {
+				return false;
+				
+			}
+		}
+
+	 public String addAirport(Airport airport) {
+		 if(airport.getAirportId().isEmpty()|| airport.getAirportName().isEmpty()|| airport.getAirportLocation().isEmpty()) {
+				return "Please fill all feilds";
+			}
+			else {
+				airportrepo.save(airport);
+				return "Airport Added Successfully "+airport.getAirportId();
+			}
+	 }
+	 public boolean modifyAirport(String airportid,Airport airport) {
+		 if(airport.getAirportId().isEmpty()|| airport.getAirportName().isEmpty()|| airport.getAirportLocation().isEmpty()) {
+				return false;
+			}
+		 else if (airportrepo.existsById(airportid)) {
+					 Optional<Airport> findById = airportrepo.findById(airport.getAirportId());
+						if (findById.isPresent()) {
+					        airportrepo.save(airport);
+					        }
+						return true;
+			}
+		 else {
+			 return false;
+		 }
+	 }
+	 }
+
