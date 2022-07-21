00a348022e7903e3ce325d20cb1b9f9210feb2fd
Shanmuga lakshmi <67747282+sowshan@users.noreply.github.com>
2022-07-21 09:16:17 +0530

    Add files via upload

--- flight-management-system/src/main/java/com/example/flightmanagementsystem/controllers/Flightcontroller.java
+++ flight-management-system/src/main/java/com/example/flightmanagementsystem/controllers/Flightcontroller.java
@@ -1,7 +1,10 @@
-package com.example.demo.contoller;
+package com.example.flightmanagementsystem.controllers;
 
 import java.util.List;
+
 import java.util.Random;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,20 +15,19 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.entity.Flight;
-import com.example.demo.exception.FlightNotFoundException;
-import com.example.demo.exception.ListEmptyException;
-import com.example.demo.service.Flightservice;
+import com.example.flightmanagementsystem.entity.Flight;
+import com.example.flightmanagementsystem.entity.User;
+import com.example.flightmanagementsystem.exceptions.FlightNotFoundException;
+import com.example.flightmanagementsystem.exceptions.ListEmptyException;
+import com.example.flightmanagementsystem.exceptions.RecordNotFoundException;
+import com.example.flightmanagementsystem.services.Flightservice;
 
 @RestController
 public class Flightcontroller {
-	
 		@Autowired
 		Flightservice flightservice;
-
    		@PostMapping(value= "/flight/addflight")
-   	    public String addCustomer(@RequestBody Flight flight)
-   	    
+   	    public String addCustomer(@RequestBody Flight flight) 	    
    	  {
    		 Random rand = new Random();
    		 int resRandom = rand.nextInt((9999 - 100) + 1) + 10;
@@ -33,43 +35,35 @@ public class Flightcontroller {
    			//if(flight.getCarrierName().length()==0||flight.getFlightModel().length()==0) throw new NullPointerException();
    			/*{
    				return "Column is Empty";
-   			}*/
-				
-   			
+   			}*/		
    			flightservice.addflight(flight);
    			return "Created successfully";
    	   }
-
    	 @GetMapping(value="/viewFlights")
-	    public List<Flight> fetchflightList() 
+	    public List<Flight> viewAllFlights() throws ListEmptyException 
 	    {   
-		 if(flightservice.fetchFlightList().isEmpty()) throw new ListEmptyException();
-	  	 	return flightservice.fetchFlightList();
-	  	 	
-	     }
-   		   	
+		 if(flightservice.viewAllFlights().isEmpty()) throw new ListEmptyException();
+	  	 	return flightservice.viewAllFlights();	  	 	
+	     }	   	
    	@GetMapping(value= "/viewFlightById/{flightId}")
-
 	public List<Flight> viewFlightByID(@PathVariable("flightId")String flightId)
 	{ 
 		if(flightservice.viewFlightById(flightId).isEmpty()) throw new FlightNotFoundException();
 		System.out.println("Fetched Successfully");
-		return flightservice.viewFlightById(flightId);
-		
+		return flightservice.viewFlightById(flightId);		
 	}
-/*
    	 @DeleteMapping(value="/deleteFlight/{flightId}")
    	 public String deleteFlight(@PathVariable String flightId) throws FlightNotFoundException {
-   		 if(flightservice.viewAllFlight().isEmpty()) 
+   		 if(flightservice.viewAllFlights().isEmpty()) 
    			{
    				System.out.println("flightid is not matched");
    		   		throw new FlightNotFoundException();
    		   	}
    		 flightservice.deleteFlight(flightId);
    		 return "Deleted Successfully";
-   	 }*/
+   	 }
     //delete by userId
-		@DeleteMapping(value="delete/flight/{flightId}")
+/*		@DeleteMapping(value="delete/flight/{flightId}")
 	   public String deleteFlight(@PathVariable("flightId") String flightId)
 	   
 	   {
@@ -78,12 +72,28 @@ public class Flightcontroller {
 		  flightservice.deleteFlight(flightId);
 	       return "Deleted Successfully";
 	 }
-
-   	 @PutMapping(value="/updateFlight")
-   	 public String modifyflight(@RequestBody Flight flight) {
-   	  flightservice.modifyFlight(flight);
-   	return "updated succesfully";
-   	 }
+*/
+   /*	@PutMapping(value="/updateFlight/{flightId}")
+   	 public String modifyflight(@PathVariable(value="flightid") String flightId, @Valid @RequestBody Flight flight)  
+   	throws RecordNotFoundException{
+   		//if(flightservice.viewFlightById(flightId).isEmpty()) throw new FlightNotFoundException();
+		//System.out.println("Fetched Successfully");
+		//return flightservice.viewFlightById(flightId);
+   	  return flightservice.modifyFlight(flightId,flight);
+   	}*/
+   	@PutMapping(value="/updateFlight/{id}")
+    public String updateAirport(@PathVariable(value = "id") String flightId, @Valid @RequestBody Flight flight)
+    {
+   		
+   		boolean x=flightservice.modifyFlight(flightId,flight);
+   		if(x) {
+   			return "Updated Successfully";
+   		}
+   		else {
+   			return "Fill all the fields and check FlightId";
+   		}
+   	}
+   		   
 }
 
 			  
